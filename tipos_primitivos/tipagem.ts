6cebc2a3135d81179ef27791593f3c85972210f4
Sara <saramagalhaes242@gmail.com>
2026-07-24 14:51:18 -0300

    primeiro commit

---
+++ tipos_primitivos/tipagem.ts
@@ -0,0 +1,13 @@
+let username: string
+
+username = "Sara"
+// username = 15
+// username = true
+
+let total: number
+total = 3
+total = 7.5
+
+let isLoading: boolean
+isLoading = true
+isLoading = false
